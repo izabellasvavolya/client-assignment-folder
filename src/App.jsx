e6d7d90fe52afbbd42b09e3063253f1b7079d49f
izabellasvavolya <izabellasv@mta.ac.il>
2026-07-20 22:37:48 +0300

    manage table data with react state

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,39 @@ export default function App() {
   /* We'll use useState so that React can remember the table data
   and update cell when it changes */
   const [tableData, setTableData] = useState(() => createMockTableData(100));
+
+  function handleCellChange(rowId, columnId, newValue) {
+    setTableData((prevTableData) => {
+      // copy the previous table data
+      const newTableData = { ...prevTableData.data};
+
+      // find the row that was changed
+      const oldRow = newTableData[rowId];
+
+      if (!oldRow) { 
+        return prevTableData; // row not found, return previous data
+      }
+
+      // copy changed row and update value
+      newTableData[rowId] = { ...oldRow, [columnId]: newValue, };
+
+      // return new table data
+      return {
+        ...prevTableData,
+        data: newTableData,
+      };
+    });
+  }
+
   return (
     <main>
 
     <h1>Dynamic Table</h1>
 
-    <EditableTable tableData={tableData} />
-    {/* pass table data to table component */}
+   <EditableTable
+   tableData={tableData}
+   onCellChange={handleCellChange}
+    />
     </main>
   );
 }
