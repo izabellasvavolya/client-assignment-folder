b9df4a0bbcddfb3418434de482f5838d2ed2b954
izabellasvavolya <izabellasv@mta.ac.il>
2026-07-20 21:37:08 +0300

    init editable table component

---
+++ src/EditableTable.jsx
@@ -0,0 +1,11 @@
+export default function EditableTable({ tableData })
+{
+    const columns = tableData.columns;
+    const data = tableData.data;
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
