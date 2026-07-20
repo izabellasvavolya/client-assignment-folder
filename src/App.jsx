b96d69a248955431cc2676b7c393b9f0d28b6c92
izabellasvavolya <izabellasv@mta.ac.il>
2026-07-20 22:16:03 +0300

    pass table data to table component as prop, call creatmocktabledata function

--- src/App.jsx
+++ src/App.jsx
@@ -7,3 +7,18 @@ import EditableTable from './EditableTable.jsx'
 import { createMockTableData } from './mockData.js'
 
 import './App.css'
+
+// ccreate mock data for table
+const tableData = createMockTableData(100);
+
+export default function App() {
+  return (
+    <main>
+
+    <h1>Dynamic Table</h1>
+
+    <EditableTable tableData={tableData} />
+    {/* pass table data to table component */}
+    </main>
+  );
+}
